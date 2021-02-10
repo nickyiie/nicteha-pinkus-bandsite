ed8b4501d8919bc487fc3ca9566412476dbe4ddb
Nicteha Pinkus <nictehapinkus@gmail.com>
2021-02-09 21:28:41 -0800

    created scss for shows table and fiddles with HTML ideas of how to create the mobile version of the table

---
+++ sprint-two/scripts/shows.js
@@ -0,0 +1,40 @@
+/* <section class="shows">
+      <h2 class="shows__title">Shows</h2>
+      <div class ="shows__boxes">
+        <div class="shows__box">
+          <h3 class="shows__label">DATE</h3>
+          <p class="shows__date">Mon Dec 17 2018</p>
+          <h3 class="shows__label">VENUE</h3>
+          <p class="shows__details">Ronald Lane</p>
+          <h3 class="shows__label">LOCATION</h3>
+          <p class="shows__details">San Fransisco, CA</p>
+          <p class="shows__button">BUY TICKETS</p>
+        </div>
+      </div>
+      </section>
+
+    <section class="shows">
+      <h2 class="shows__title">Shows</h2>
+      <table class ="shows__table">
+        <tr>
+          <th class="shows__label">DATE</th>
+        </tr>
+        <tr>
+          <td class="shows__date">Mon Dec 17 2018</td>
+        </tr>
+        <tr>
+          <th class="shows__label">VENUE</th>
+        </tr>
+        <tr>
+          <td class="shows__details">Ronald Lane</td>
+        </tr>
+        <tr>
+          <th class="shows__label">LOCATION</th>
+        </tr>
+        <tr>
+          <td class="shows__details">San Fancisco, CA</td>
+        </tr>
+      </table>
+      <p class="shows__button">BUY TICKETS</p>
+      </section>
+*/
