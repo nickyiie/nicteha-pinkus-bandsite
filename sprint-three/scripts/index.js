f85ead05ce5b2a4bbe3df3cb4a68a00389566b9f
Nicteha Pinkus <nictehapinkus@gmail.com>
2021-02-22 17:54:48 -0800

    semi last commit

--- sprint-three/scripts/index.js
+++ sprint-three/scripts/index.js
@@ -9,155 +9,90 @@ title.classList.add('comments__title');
 title.innerText = "Join The Conversation"
 comments.appendChild(title);
 
-// let interactiveForm = document.createElement('div');
-// interactiveForm.classList.add('comments__interactive');
-// comments.appendChild(interactiveForm);
+let form = document.querySelector('.comments__interactive');
+comments.appendChild(form);
 
-// let userImageActive = document.createElement('img');
-// userImageActive.classList.add('comments__image');
-// userImageActive.setAttribute('src', "./assets/images/Mohan-muruge.jpg");
-// userImageActive.setAttribute('rel', 'user avatar');
-// interactiveForm.appendChild(userImageActive);
-
-// let form = document.createElement('form');
-// form.classList.add('comments__form');
-// interactiveForm.appendChild(form);
-
-// let nameLabel = document.createElement('label');
-// nameLabel.classList.add('comments__label');
-// nameLabel.innerText = 'NAME';
-// nameLabel.setAttribute('for', 'name');
-// form.appendChild(nameLabel);
-
-// let nameInput = document.createElement('input');
-// nameInput.classList.add('comments__input');
-// setAttributes (nameInput, {'type': 'text', 'name': 'name', 'placeholder': 'Your name...'});
-// form.appendChild(nameInput);
-
-// let commentsLabel = document.createElement('label');
-// commentsLabel.classList.add('comments__label');
-// commentsLabel.setAttribute('for', 'comments');
-// commentsLabel.innerText = 'COMMENT';
-// form.appendChild(commentsLabel);
-
-// let textBox = document.createElement('textarea');
-// textBox.classList.add('comments__textbox');
-// setAttributes (textBox, {'type': 'text', 'name': 'comment', 'placeholder': 'Add a comment'});
-// form.appendChild(textBox);
-
-// let button = document.createElement('button');
-// button.classList.add('comments__button');
-// button.setAttribute('type', 'submit')
-// button.innerText = 'COMMENT';
-// form.appendChild(button);
-
-let cDate= new Date();
-
-let commentDate = cDate.getMonth() + "/" + cDate.getDate() + "/" + cDate.getFullYear();
-
-
-// userComments.forEach(function (commentsDisplayBox) {
-
-//   let commentBox = document.createElement('div');
-//   commentBox.classList.add('comments__display');
-//   comments.appendChild(commentBox);
-
-//   let image = document.createElement('img');
-//   image.classList.add('comments__image');
-//   image.setAttribute('src', './assets/images/Mohan-muruge.jpg');
-//   commentBox.appendChild(image);
-
-//   let userInfo = document.createElement('div');
-//   userInfo.classList.add('comments__old');
-//   commentBox.appendChild(userInfo);
-
-//   let headingBox = document.createElement('div');
-//   headingBox.classList.add('comments__heading');
-//   userInfo.appendChild(headingBox);
-
-//   let userName = document.createElement('h3');
-//   userName.classList.add('comments__username');
-//   userName.innerText = commentsDisplayBox.username;
-//   headingBox.appendChild(userName);
-
-//   let commentDate = document.createElement('p');
-//   commentDate.classList.add('comments__date');
-//   commentDate.innerText = commentsDisplayBox.date;
-//   headingBox.appendChild(commentDate);
-
-//   let comment = document.createElement('p');
-//   comment.classList.add('comments__comment');
-//   comment.innerText = commentsDisplayBox.comment;
-//   userInfo.appendChild(comment);
-
-//   return commentBox;
-// } 
-// ); 
-
-
-let formInteractive = document.querySelector('form');
-console.log(formInteractive);
-formInteractive.addEventListener("submit", (event) => {
+let interactiveForm = document.querySelector('form');
+console.log(interactiveForm);
+interactiveForm.addEventListener("submit", (event) => {
   event.preventDefault();
   
   let newComment = {
-    username: event.target.name.value,
-    date: commentDate,
-    comment: event.target.comment.value
+    'name': event.target.name.value,
+    'comment': event.target.comment.value
   }
-      function displayComment() {
 
-        let newestCommentBox = document.createElement('div');
-        newestCommentBox.classList.add('comments__display');
-        interactiveForm.insertAdjacentElement('afterend', newestCommentBox);
-    
-    
-        let image = document.createElement('img');
-        image.classList.add('comments__image');
-        image.setAttribute('src', './assets/images/Mohan-muruge.jpg');
-        newestCommentBox.appendChild(image);
-    
-        let userInfo = document.createElement('div');
-        userInfo.classList.add('comments__old');
-        newestCommentBox.appendChild(userInfo);
-    
-        let headingBox = document.createElement('div');
-        headingBox.classList.add('comments__heading');
-        userInfo.appendChild(headingBox);
-    
-        let userName = document.createElement('h3');
-        userName.classList.add('comments__username');
-        userName.innerText = newComment.username;
-        headingBox.appendChild(userName);
-    
-        let commentDate = document.createElement('p');
-        commentDate.classList.add('comments__date');
-        commentDate.innerText = newComment.date;
-        headingBox.appendChild(commentDate);
-    
-        let newestcomment = document.createElement('p');
-        newestcomment.classList.add('comments__comment');
-        newestcomment.innerText = newComment.comment;
-        userInfo.appendChild(newestcomment);
+  function displayComment(comment) {
 
+    let commentBox = document.createElement('div');
+    commentBox.classList.add('comments__display');
+    form.insertAdjacentElement('afterend', commentBox);
+  
+    let image = document.createElement('img');
+    image.classList.add('comments__image');
+    image.setAttribute('src', './assets/images/Mohan-muruge.jpg');
+    commentBox.appendChild(image);
+  
+    let userInfo = document.createElement('div');
+    userInfo.classList.add('comments__old');
+    commentBox.appendChild(userInfo);
+  
+    let headingBox = document.createElement('div');
+    headingBox.classList.add('comments__heading');
+    userInfo.appendChild(headingBox);
+  
+    let userName = document.createElement('h3');
+    userName.classList.add('comments__username');
+    userName.classList.add('comments__username-new');
+    userName.innerText = newComment.name;
+    headingBox.appendChild(userName);
+  
+    let time = new Date
+    let cDate = time.getMonth() + "/" + time.getDate() + "/" + time.getFullYear();
+
+    let commentDate = document.createElement('p');
+    commentDate.classList.add('comments__date');
+    commentDate.innerText = cDate;
+    headingBox.appendChild(commentDate);
+  
+    let newestComment = document.createElement('p');
+    newestComment.classList.add('comments__comment');
+    newestComment.innerText = newComment.comment;
+    userInfo.appendChild(newestComment);
+  
+    return commentBox;
+  };
+  displayComment();
+  interactiveForm.reset();
+
+axios 
+  .post('https://project-1-api.herokuapp.com/comments?api_key=nicky', newComment)
+  .then (response => {
+console.log(response)
     
-        return newestCommentBox
-      };
-      
-      // userComments.unshift(newComment);
-      displayComment();
-      form.reset();
-    }
-    );
-let form = document.querySelector('.comments__interactive');
-let commentArray = axios 
+    })
+    .catch (error => {
+      console.log(error);
+      let errorMessage = document.createElement('h1');
+      errorMessage.innerText = 'Sorry, comments are currently not available';
+      interactiveForm.appendChild(errorMessage);
+    })  
+    });
+
+
+axios 
   .get('https://project-1-api.herokuapp.com/comments?api_key=nicky')
   .then (response => {
     commentsArray = response.data;
-    console.log(commentsArray);
-    
+     
+commentsArray.forEach( 
+  
+  function (commentObj) {
 
-commentsArray.forEach(function (commentsDisplayBox) {
+    let time = new Date (commentObj.timestamp);
+    console.log(time);
+    let cDate = time.getMonth() + "/" + time.getDate() + "/" + time.getFullYear();
+    console.log(cDate);
 
   let commentBox = document.createElement('div');
   commentBox.classList.add('comments__display');
@@ -178,35 +113,26 @@ commentsArray.forEach(function (commentsDisplayBox) {
 
   let userName = document.createElement('h3');
   userName.classList.add('comments__username');
-  userName.innerText = commentsDisplayBox.name;
+  userName.innerText = commentObj.name;
   headingBox.appendChild(userName);
 
   let commentDate = document.createElement('p');
   commentDate.classList.add('comments__date');
-  commentDate.innerText = commentsDisplayBox.date;
+  commentDate.innerText = cDate;
   headingBox.appendChild(commentDate);
 
   let comment = document.createElement('p');
   comment.classList.add('comments__comment');
-  comment.innerText = commentsDisplayBox.comment;
+  comment.innerText = commentObj.comment;
   userInfo.appendChild(comment);
 
   return commentBox;
-} 
-)
+})
 }).catch(error => {
-  
   let errorMessage = document.createElement('h1');
   errorMessage.innerText = 'Sorry, comments are currently not available';
   interactiveForm.appendChild(errorMessage);
 });
-
-
-function setAttributes(el, attribute) {
-  for (let key in attribute) {
-  el.setAttribute(key, attribute[key]);
-}
-};
 
 
 
--- sprint-three/scripts/shows.js
+++ sprint-three/scripts/shows.js
@@ -1,37 +1,3 @@
-// const upcomingShows = [
-//    {
-//     date: 'Mon Dec 17 2018', 
-//     venue: 'Ronald Lane',      
-//     location: 'San Francisco, CA'
-//   },
-//   {
-//     date: 'Tue Jul 18 2019', 
-//     venue: 'Pier 3 East',      
-//     location: 'San Francisco, CA'
-//   },
-//   {
-//     date: 'Fri Jul 22 2019', 
-//     venue: 'View Loungue',      
-//     location: 'San Francisco, CA'
-//   },
-//   {
-//     date: 'Sat Aug 12 2019', 
-//     venue: 'Hyatt Agency',      
-//     location: 'San Francisco, CA'
-//   },
-//   {
-//     date: 'Fri Sep 05 2019', 
-//     venue: 'Moscow Center',      
-//     location: 'San Francisco, CA'
-//   },
-//   {
-//     date: 'Wed Aug 11 2019', 
-//     venue: 'Pres Club',      
-//     location: 'San Francisco, CA'
-//   },
-// ];
-
-
 let hero = document.querySelector('.hero');
 let shows = document.createElement('section');
 shows.classList.add('shows');
